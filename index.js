d115064e213a58d2f7c8f3deb5fe6d78bc873665
anik <woshikuzzaman@gmail.com>
2019-11-16 20:56:18 +0600

    Update

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const cluster = require("cluster")
 
 if (cluster.isMaster) {
-    let numCPUs = require('os').cpus().length
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork()
-    }
-
+    let env = "development"
     if (process.env.NODE_ENV === "production") {
+        env = "production"
         cluster.on('exit', (worker) => {
-            cluster.fork()
+            cluster.fork({
+                NODE_ENV: env
+            })
         })
     }
+
+    let numCPUs = require('os').cpus().length
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork({
+            NODE_ENV: env
+        })
+    }
+
 } else {
     //global declaration
     global.join = require("path").join;
